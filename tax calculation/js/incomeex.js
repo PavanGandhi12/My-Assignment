328f9064b006de791a8b828f5f97f3d74bcbd442
PavanGandhi12 <pavangandhis12@gmail.com>
2023-04-04 18:40:29 +0530

    first commit

---
+++ tax calculation/js/incomeex.js
@@ -0,0 +1,17 @@
+
+
+let calculate(){
+
+    let income = parseFloat(document.getElementById("income").value);
+ debugger
+    document.getElementById(
+        "gender").selectedIndex = "";
+        debugger
+        
+if( Grossincome <= 50000){
+
+   
+
+}  else if(Grossincome <=50001 && Grossincome <=150000 && Gender === Male)
+
+}
